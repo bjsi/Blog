75613c2b0d95e7b9d2d23ff1e329e8d0feeead4a
james <james@DESKTOP-JDJA6NK>
2020-05-19 05:13:11 +0100

    env vars

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-
-class Config:
-    FLASK_DEBUG = False
-    NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
-    NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 import os
 from app import create_app
+from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
+    load_dotenv()
     app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
