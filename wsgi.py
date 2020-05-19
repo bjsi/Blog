c0e255e5655792e208337bed7e2e68be6ff38438
james <james@DESKTOP-JDJA6NK>
2020-05-19 05:34:38 +0100

    moved app

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,10 @@ from app import create_app
 from dotenv import load_dotenv
 
 
+app = create_app()
+
+
 if __name__ == "__main__":
     load_dotenv()
-    app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
